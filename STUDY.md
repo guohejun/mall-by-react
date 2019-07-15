89001bae5b6de0260f0bfc6f3bfdb18914bfaa01
qiye <13866288016@163.com>
2019-07-16 00:24:24 +0800

    fix

--- STUDY.md
+++ STUDY.md
@@ -1,10 +1,53 @@
 ## 知识记述
-1.react-router 和 react-router-dom的区别
-2.引用antd-mobile插件，使用babel-plugin-import插件，在package.json文件中设置全局引用antd-mobile的样式文件
-3.preact和react的区别
 
-## vsCode插件列表
-https://blog.csdn.net/weixin_39876634/article/details/88562183
+1. react-router 和 react-router-dom 的区别
+2. 引用 antd-mobile 插件，使用 babel-plugin-import 插件，在 package.json 文件中设置全局引用 antd-mobile 的样式文件
+3. preact 和 react 的区别
+4. react 组件中绑定 this 的四种方式
 
-## antd-mobile官方文档
-https://mobile.ant.design/docs/react/introduce-cn
+   - 第四种：
+     事件定义：采用箭头函数定义；事件调用：采用箭头函数包裹
+
+   ```
+   onLinkTo = (e, item) => {
+    console.log(e, item)
+    this.props.history.push({
+      pathname: item.appUrl
+    })
+   }
+   ```
+
+   ```
+   <Flex>
+    {this.state.noticeList.map((item, index) => (
+        <Flex.Item key={index} className="cardItem" onClick={(e) => this.onLinkTo(e, item)}>
+        <img src="{item.imgUrl}" alt="" className="cardItem__img"/>
+        <div className="cardItem__right">
+            <p className="title">{item.title}</p>
+        </div>
+        </Flex.Item>
+    ))}
+    </Flex>
+   ```
+
+---
+
+## vsCode 插件列表
+
+- [vsCode 插件列表](https://blog.csdn.net/weixin_39876634/article/details/88562183)
+
+---
+
+## antd-mobile 官方文档
+
+- [antd-mobile 官方文档](https://mobile.ant.design/docs/react/introduce-cn)
+
+---
+
+## 使用 Tab 键实现代码自动补全设置：
+
+- 在 Settings->Workbench->Appearance->Edit in settings.json 文件根级，添加代码：
+  "emmet.triggerExpansionOnTab":true
+  保存即可。
+
+---
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,76 @@
-import React, { Component } from 'react';
-import logo from '@src/logo.svg'
-import './index.sass'
-import {Button, TabBar} from 'antd-mobile'
+import React, { Component } from "react";
+import logo from "@src/logo.svg";
+import "./index.sass";
+import { Button, TabBar, Flex, SearchBar } from "antd-mobile";
 
 class Home extends Component {
-    constructor() {
-        super()
-        this.state = {
-            
-        }
-    }
-
-    render() {
-        return (
-            <div className="home-page">
-                <h1 className="home-title">I'm the Home Page</h1>
-                <Button type="primary" onClick={() => this.props.history.push({
-                    pathname: '/login',
-                    state: {
-                        id: 3
-                    }
-                })}>通过函数跳转</Button>
-                TabBar
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    const noticeList = [
+      { id: 1, appUrl: "/", title: "首页", imgUrl: "a.jpg" },
+      { id: 2, appUrl: "/login", title: "登录", imgUrl: "a.jpg" },
+      { id: 3, appUrl: "/user", title: "个人中心", imgUrl: "a.jpg" },
+      { id: 4, appUrl: "/404", title: "404页面", imgUrl: "a.jpg" }
+    ]
+    this.state = {
+      value: "",
+      noticeList
+    };
+  }
+  onChange = val => {
+    console.log(val);
+    this.setState({ value: val });
+  };
+  onLinkTo = (e, item) => {
+    console.log(e, item)
+    this.props.history.push({
+      pathname: item.appUrl
+    })
+  }
+  render() {
+    return (
+      <div className="home-page">
+        <SearchBar
+          value={this.state.value}
+          placeholder="Search"
+          onSubmit={value => console.log(value, "onSubmit")}
+          onClear={value => console.log(value, "onClear")}
+          onFocus={() => console.log("onFocus")}
+          onBlur={() => console.log("onBlur")}
+          onCancel={() => console.log("onCancel")}
+          showCancelButton
+          onChange={this.onChange}
+        />
+        <section className="bannerBox" />
+        <section className="cardBox">
+          <Flex>
+            {this.state.noticeList.map((item, index) => (
+              <Flex.Item key={index} className="cardItem" onClick={(e) => this.onLinkTo(e, item)}>
+                <img src="{item.imgUrl}" alt="" className="cardItem__img"/>
+                <div className="cardItem__right">
+                  <p className="title">{item.title}</p>
+                </div>
+              </Flex.Item>
+            ))}
+          </Flex>
+        </section>
+        <Button
+          type="primary"
+          onClick={() =>
+            this.props.history.push({
+              pathname: "/login",
+              state: {
+                id: 3
+              }
+            })
+          }
+        >
+          通过函数跳转
+        </Button>
+        TabBar
+      </div>
+    );
+  }
 }
 
 export default Home;
